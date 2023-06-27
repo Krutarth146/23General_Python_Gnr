cff24c710a7b0fd619ca54f80f93de2ce4c61a16
Krutarth Patel <krutarthpatel146@gmail.com>
2023-06-27 18:22:38 +0530

    Practice

---
+++ practice_27_6.py
@@ -0,0 +1,42 @@
+x = "Jhon"
+
+for i in x:
+    if i=='o':
+        pass
+    print(i)
+
+
+#  
+
+# 5 
+# 55
+# 555
+# 5555
+# 55555
+
+# step = ?, num = ?
+
+# Sum = (??)
+
+# 3
+# 33
+# 333
+# 3333
+# 33333
+
+# 3 ---> 33
+
+num = 5
+xerox = num
+step = 5
+x = 5 
+
+for i in range(step):
+    print(x)  # 555
+    x = x * 10 + xerox
+    num += x
+
+print(num)
+# Fibonaaci
+# Factorial
+# Power
