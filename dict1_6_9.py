9a640dd261a7a6409b06c17f59ed9e2d45fa8a1f
Krutarth Patel <krutarthpatel146@gmail.com>
2023-09-08 18:02:48 +0530

    Dictionary Prac.

--- dict1_6_9.py
+++ dict1_6_9.py
@@ -62,67 +62,67 @@ dict6 = dict.fromkeys(x,y)
 
 print(dict6)  # {'Name': 0, 'roll': 0, 'school': 0}
 
-dict1 = {'Name': 'Mithil', 'Roll': 802, 78: 89.9, 'address': {'City': 'Ahm', 'State': 'Guj', 'Pincode': {"standard" : [10, 90, 34, 56, 22]}}, 'Institute': 'Royal', 'School': 'HBK', 'Manoj': 'Tripathi'}
+# dict1 = {'Name': 'Mithil', 'Roll': 802, 78: 89.9, 'address': {'City': 'Ahm', 'State': 'Guj', 'Pincode': {"standard" : [10, 90, 34, 56, 22]}}, 'Institute': 'Royal', 'School': 'HBK', 'Manoj': 'Tripathi'}
 
-print(dict1.keys())   # dict_keys(['Name', 'Roll', 78, 'address', 'Institute', 'School', 'Manoj'])
-print(dict1.values())   # dict_values(['Mithil', 802, 89.9, {'City': 'Ahm', 'State': 'Guj', 'Pincode': {'standard': [10, 90, 34, 56, 22]}}, 'Royal', 'HBK', 'Tripathi'])
-print(dict1['address'].keys())   # dict_keys(['City', 'State', 'Pincode'])
-print(dict1['address'].values())   # dict_values(['Ahm', 'Guj', {'standard': [10, 90, 34, 56, 22]}])
-
-
-print(dict1.items())   # dict_items([('Name', 'Mithil'), ('Roll', 802), (78, 89.9), ('address', {'City': 'Ahm', 'State': 'Guj', 'Pincode': {'standard': [10, 90, 34, 56, 22]}}), ('Institute', 'Royal'), ('School', 'HBK'), ('Manoj', 'Tripathi')])
+# print(dict1.keys())   # dict_keys(['Name', 'Roll', 78, 'address', 'Institute', 'School', 'Manoj'])
+# print(dict1.values())   # dict_values(['Mithil', 802, 89.9, {'City': 'Ahm', 'State': 'Guj', 'Pincode': {'standard': [10, 90, 34, 56, 22]}}, 'Royal', 'HBK', 'Tripathi'])
+# print(dict1['address'].keys())   # dict_keys(['City', 'State', 'Pincode'])
+# print(dict1['address'].values())   # dict_values(['Ahm', 'Guj', {'standard': [10, 90, 34, 56, 22]}])
 
 
-for i in dict1:   # Default Keys
-    print(i, dict1[i])   # dict1['Roll']
+# print(dict1.items())   # dict_items([('Name', 'Mithil'), ('Roll', 802), (78, 89.9), ('address', {'City': 'Ahm', 'State': 'Guj', 'Pincode': {'standard': [10, 90, 34, 56, 22]}}), ('Institute', 'Royal'), ('School', 'HBK'), ('Manoj', 'Tripathi')])
 
 
-for h in dict1.values():
-    print(h)
-
-for h in dict1.items():
-    print(h,end=' ')   # ('Name', 'Mithil') ('Roll', 802) (78, 89.9) ('address', {'City': 'Ahm', 'State': 'Guj', 'Pincode': {'standard': [10, 90, 34, 56, 22]}}) ('Institute', 'Royal') ('School', 'HBK') ('Manoj', 'Tripathi')
+# for i in dict1:   # Default Keys
+#     print(i, dict1[i])   # dict1['Roll']
 
 
-for key, val in dict1.items():
-    print(key,'----> ', val)
-    if key == 'Name':
-        dict1[key] = 'SB Sir'
+# for h in dict1.values():
+#     print(h)
 
-print(dict1)
-
-
-str1 = "MISSISSIPPI"
-
-# dict1 = {'M' : 1, 'I' : 4, 'P' : 2, 'S' : 4}
-
-dict2 = {}
-
-# for ch in str1:
-#     if ch not in dict2:
-#         dict2[ch] = 1
-#     else:
-#         dict2[ch] += 1   # dict2[i] = dict2[i] + 1
+# for h in dict1.items():
+#     print(h,end=' ')   # ('Name', 'Mithil') ('Roll', 802) (78, 89.9) ('address', {'City': 'Ahm', 'State': 'Guj', 'Pincode': {'standard': [10, 90, 34, 56, 22]}}) ('Institute', 'Royal') ('School', 'HBK') ('Manoj', 'Tripathi')
 
 
-# # {'M' :  1, 'I' : 1, 'S' : 2}
+# for key, val in dict1.items():
+#     print(key,'----> ', val)
+#     if key == 'Name':
+#         dict1[key] = 'SB Sir'
 
-# print(dict2['M'])
-
-
-set1 = set(str1)
-print(set1)   # {'P', 'M', 'S', 'I'}
-
-
-dict2 = {}
-
-for i in set1:
-    count = 0
-    for j in str1:
-        if i == j:
-            count+=1
-
-    dict2.update({i : count})
+# print(dict1)
 
 
-print(dict2)
+# str1 = "MISSISSIPPI"
+
+# # dict1 = {'M' : 1, 'I' : 4, 'P' : 2, 'S' : 4}
+
+# dict2 = {}
+
+# # for ch in str1:
+# #     if ch not in dict2:
+# #         dict2[ch] = 1
+# #     else:
+# #         dict2[ch] += 1   # dict2[i] = dict2[i] + 1
+
+
+# # # {'M' :  1, 'I' : 1, 'S' : 2}
+
+# # print(dict2['M'])
+
+
+# set1 = set(str1)
+# print(set1)   # {'P', 'M', 'S', 'I'}
+
+
+# dict2 = {}
+
+# for i in set1:
+#     count = 0
+#     for j in str1:
+#         if i == j:
+#             count+=1
+
+#     dict2.update({i : count})
+
+
+# print(dict2)
