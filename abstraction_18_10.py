285ebf30e75ef835b110b7139636d1cfa5accb00
Krutarth Patel <krutarthpatel146@gmail.com>
2023-10-18 18:18:43 +0530

    Abstarction 18_10

--- abstraction_18_10.py
+++ abstraction_18_10.py
@@ -62,12 +62,60 @@ mithil.get_interstRates(2.5)
 nupur.get_interstRates(4)
 
 
+
+
+
+# -------------------------------------------------------------------------------
 #  Join Tuples if similar initial element
 
 que = [(10,20), (10,30), (5,7), (7,5), (5,9,5), (70,7), (7,10)]
 # ans = [(10,20,30), (5,7,9), (7,5,10), (70,7)]
 
 
-[for i in range(len(que)) for j in range(i+1, len(que)) if que[i][0] == que[j][0] for x in que[]]
+# [ for i in range(len(que)) for j in range(i+1, len(que)) if que[i][0] == que[j][0]]
 
-for i in range(len(que))
+x = list(set([j[0] for j in que]))
+
+main = []
+for i in x:
+    temp = []
+    count = 0
+    for j in range(len(que)):
+        if i == que[j][0]:
+            count+=1
+            temp.extend(que[j])
+        if count == 0:
+            temp.extend(que[j])
+    if len(temp) != 0:
+        main.append(set(temp))
+
+print(main)
+
+
+
+dev = [list({v for subtup in list1 if subtup[0] == k for v in subtup}) for k in start]
+print(dev)   # [[90, 5, 6, 7], [9, 5, 6]]
+
+
+# ------------------------------------
+
+
+tup1 = ((1,9,4), (8,3,2), (9,1,2))
+k = 4
+
+ans = ((5,13,8), (12,7,6), (13,5,6))
+
+
+# --------------------------------------
+
+
+# Encryption - Decryption
+
+# Jalp Panchal ----> lXIM XXXXXX
+
+# capital ---> +2 (Small)   C --> e
+# small   ---> -3 (Capital)  # c ---> Z
+
+print(ord('A'))   # 65
+
+print(chr(ord('A') + 2))   # C
